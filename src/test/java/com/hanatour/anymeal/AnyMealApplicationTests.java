5ad83f122545d7cacc1962addd105b855707d7a9
khu3000@naver.com <%basic725>
2021-06-30 19:49:42 +0900

    init

---
+++ src/test/java/com/hanatour/anymeal/AnyMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanatour.anymeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
