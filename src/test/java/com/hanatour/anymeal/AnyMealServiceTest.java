93c6ca0a95e28476ee752b1790abb5e6e28d7db2
khu3000@naver.com <%basic725>
2021-07-28 22:31:22 +0900

    김현욱 : 4개 포인트 기준 api 호출 적용

--- src/test/java/com/hanatour/anymeal/AnyMealServiceTest.java
+++ src/test/java/com/hanatour/anymeal/AnyMealServiceTest.java
@@ -1,5 +1,6 @@
 package com.hanatour.anymeal;
 
+import com.hanatour.anymeal.geocalc.Point;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,20 +30,20 @@ class AnyMealServiceTest {
 
   @Test
   void getRestaurantNearAllPagesTest() {
-    List<Restaurant> rest = AnyMealService.getRestaurantNearAllPages("126.983618", "37.572043", "distance", "100", "FD6", 1);
+    List<Restaurant> rest = AnyMealService.getRestaurantNearAllPages(Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 1);
     System.out.println(rest);
     System.out.println(rest.size());
   }
 
   @Test
   void getRestaurantNearDetailTest() {
-    CategoryResponse response = AnyMealService.getRestaurantNear("126.983618", "37.572043", "distance", "100", "FD6", 3);
+    CategoryResponse response = AnyMealService.getRestaurantNear(Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 3);
     System.out.println(response);
   }
 
   @Test
   void invalidPositionTest() {
-    List<Restaurant> rest = AnyMealService.getRestaurantNearAllPages("999.999999", "99.999999", "distance", "500", "FD6", 1);
+    List<Restaurant> rest = AnyMealService.getRestaurantNearAllPages(Point.at("99.999999", "999.999999"), "distance", 500, "FD6", 1);
     System.out.println(rest);
     System.out.println(rest.size());
   }
@@ -51,7 +52,7 @@ class AnyMealServiceTest {
   @DisplayName("깊은 산속에는 식당이 없다")
   void noRestaurantInMountain() {
     //https://www.google.com/maps/@38.3774456,128.1937431,11.88z
-    List<Restaurant> rest = AnyMealService.getRestaurantNearAllPages("128.1937431", "38.3774456", "distance", "100", "FD6", 1);
+    List<Restaurant> rest = AnyMealService.getRestaurantNearAllPages(Point.at("38.3774456", "128.1937431"), "distance", 100, "FD6", 1);
     assertTrue(rest.isEmpty());
   }
 
