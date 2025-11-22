f7c6baf30b696944035343198b3017714e877e00
김유진 <ek@hanatour.com>
2025-11-22 11:58:28 +0900

    fix test fail

--- src/test/java/com/hanatour/anymeal/AnyMealServiceTest.java
+++ src/test/java/com/hanatour/anymeal/AnyMealServiceTest.java
@@ -18,50 +18,54 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class AnyMealServiceTest {
 
-  @Autowired
-  AnyMealService anyMealService;
+    @Autowired
+    AnyMealService anyMealService;
 
-  @BeforeEach
-  void setUp() {
-  }
+    @BeforeEach
+    void setUp() {
+    }
 
-  @AfterEach
-  void tearDown() {
-  }
+    @AfterEach
+    void tearDown() {
+    }
 
-  @Test
-  void getRestaurantNearTest() {
-    Optional<Restaurant> rest = anyMealService.getRestaurantNear("126.983618", "37.572043");
-    System.out.println(rest);
-    assertTrue(rest.isPresent());
-  }
+    @Test
+    void getRestaurantNearTest() {
+        Optional<Restaurant> rest = anyMealService.getRestaurantNear("126.983618", "37.572043");
+        System.out.println(rest);
+        assertTrue(rest.isPresent());
+    }
 
-  @Test
-  void getRestaurantNearAllPagesTest() {
-    List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 1);
-    System.out.println(rest);
-    System.out.println(rest.size());
-  }
+    @Test
+    void getRestaurantNearAllPagesTest() {
+        final List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
+            Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 1);
+        System.out.println(rest);
+        System.out.println(rest.size());
+    }
 
-  @Test
-  void getRestaurantNearDetailTest() {
-    CategoryResponse response = anyMealService.getRestaurantNear(Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 3);
-    System.out.println(response);
-  }
+    @Test
+    void getRestaurantNearDetailTest() {
+        final CategoryResponse response = anyMealService.getRestaurantNear(
+            Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 3);
+        System.out.println(response);
+    }
 
-  @Test
-  void invalidPositionTest() {
-    List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(Point.at("99.999999", "999.999999"), "distance", 500, "FD6", 1);
-    System.out.println(rest);
-    System.out.println(rest.size());
-  }
+    @Test
+    void invalidPositionTest() {
+        List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
+            Point.at("99.999999", "999.999999"), "distance", 500, "FD6", 1);
+        System.out.println(rest);
+        System.out.println(rest.size());
+    }
 
-  @Test
-  @DisplayName("깊은 산속에는 식당이 없다")
-  void noRestaurantInMountain() {
-    //https://www.google.com/maps/@38.3774456,128.1937431,11.88z
-    List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(Point.at("38.3774456", "128.1937431"), "distance", 100, "FD6", 1);
-    assertTrue(rest.isEmpty());
-  }
+    @Test
+    @DisplayName("깊은 산속에는 식당이 없다")
+    void noRestaurantInMountain() {
+        //https://www.google.com/maps/@38.3774456,128.1937431,11.88z
+        List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
+            Point.at("38.3774456", "128.1937431"), "distance", 100, "FD6", 1);
+        assertTrue(rest.isEmpty());
+    }
 
 }
