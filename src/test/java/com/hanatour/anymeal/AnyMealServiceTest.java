ee755a0ca04e7eaff0ba7ad340202bac64f0e615
김유진 <ek@hanatour.com>
2025-11-22 12:15:38 +0900

    refactor test

--- src/test/java/com/hanatour/anymeal/AnyMealServiceTest.java
+++ src/test/java/com/hanatour/anymeal/AnyMealServiceTest.java
@@ -1,10 +1,6 @@
 package com.hanatour.anymeal;
 
 import com.hanatour.anymeal.geocalc.Point;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,17 +17,9 @@ class AnyMealServiceTest {
     @Autowired
     AnyMealService anyMealService;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
     void getRestaurantNearTest() {
-        Optional<Restaurant> rest = anyMealService.getRestaurantNear("126.983618", "37.572043");
+        final Optional<Restaurant> rest = anyMealService.getRestaurantNear("126.983618", "37.572043");
         System.out.println(rest);
         assertTrue(rest.isPresent());
     }
@@ -42,6 +30,7 @@ class AnyMealServiceTest {
             Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 1);
         System.out.println(rest);
         System.out.println(rest.size());
+        assertFalse(rest.isEmpty());
     }
 
     @Test
@@ -49,21 +38,23 @@ class AnyMealServiceTest {
         final CategoryResponse response = anyMealService.getRestaurantNear(
             Point.at("37.572043", "126.983618"), "distance", 100, "FD6", 3);
         System.out.println(response);
+        assertNotNull(response);
     }
 
     @Test
     void invalidPositionTest() {
-        List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
+        final List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
             Point.at("99.999999", "999.999999"), "distance", 500, "FD6", 1);
         System.out.println(rest);
         System.out.println(rest.size());
+        assertFalse(rest.isEmpty());
     }
 
     @Test
     @DisplayName("깊은 산속에는 식당이 없다")
     void noRestaurantInMountain() {
         //https://www.google.com/maps/@38.3774456,128.1937431,11.88z
-        List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
+        final List<Restaurant> rest = anyMealService.getRestaurantNearAllPages(
             Point.at("38.3774456", "128.1937431"), "distance", 100, "FD6", 1);
         assertTrue(rest.isEmpty());
     }
